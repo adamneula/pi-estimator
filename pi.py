91e7ebf362b8821c6d05e660ea7c58e66104c19b
Adam <adamneula@gmail.com>
2025-03-12 17:33:47 -0700

    some massive changes

--- pi.py
+++ pi.py
@@ -1,10 +1,13 @@
 import random
+from tqdm import tqdm
 
 count: int = 0
-num = int(input("How many iterations do you want\n"))
-for _ in range(num):
-	x = random.random()
-	y = random.random()
-	if x ** 2 + y ** 2 <= 1:
-		count += 1
-print(4*count/num)	
+num = int(input("How many iterations do you want?\n"))
+
+for i in tqdm(range(num), desc="Status: "):
+    x = random.random()
+    y = random.random()
+    if x ** 2 + y ** 2 <= 1:
+        count += 1
+
+print(4 * count / num)
